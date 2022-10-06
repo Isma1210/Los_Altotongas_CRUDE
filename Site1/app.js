f4ac1e3a81f9e88208e2112a814046a060dac9a6
DarknessPrince1210 <diaz.tiburcio.ismael@gmail.com>
2022-10-06 09:37:47 -0500

    Initial commit

---
+++ Site1/app.js
@@ -0,0 +1,111 @@
+const express=require('express');
+const mysql=require('mysql2');
+var bodyParser=require('body-parser');
+var app=express();
+
+const path=require('path');
+const { response } = require('express');
+
+var con=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Cami3105+',
+    database:'altotonga'
+})
+
+// response.sendFile(path.resolve(__dirname,'pagOrder.html'));
+//INSERT INTO pedido (nombre,correo,producto) VALUES("Carlos","carlos@gmail.com","Sandwich de Pollo");
+
+app.use(express.static('public'))
+
+con.connect();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+app.post('/addOrder',(req,res)=>{
+    let nombre=req.body.nombre
+    let correo=req.body.email
+    let producto=req.body.producto
+
+    con.query('INSERT INTO pedido(nombre,correo,producto) VALUES("'+nombre+'","'+correo+'","'+producto+'")',(err,respuesta,fields)=>{
+
+        if (err)return console.log("Error",err)
+        console.log(nombre,correo,producto)
+        
+        //return res.sendFile(path.resolve(__dirname,'public/pagOrder.html'));
+        return res.send(`<a href="index.html">Inicio</a>
+        <h1>El pedido de ${nombre} es: ${producto}</h1>`)
+
+    })
+
+})
+
+
+app.post('/delOrder',(req,res)=>{
+    let nomCustomer=req.body.nomClient;
+    let nomProduct=req.body.nomProduct;
+
+    con.query('DELETE FROM pedido WHERE nombre=("'+nomCustomer+'") AND producto=("'+nomProduct+'")',(err,respuesta,field)=>{
+        if(err) return console.log('ERROR:',err)
+
+        
+
+        return res.send(`
+        <a href="pagOrder.html">Inicio</a>
+        <h1>Pedido de ${nomCustomer} eliminado... ${nomProduct}</h1>`)
+    })
+})
+
+app.get('/getOrders',(req,res)=>{
+    
+    con.query('SELECT *FROM pedido',(err,respuesta,field)=>{
+        if(err) return console.log('ERROR:',err)
+
+        var userHTML=``
+        var i=0
+        console.log(respuesta)
+        userHTML+=`
+        <a href="pagOrder.html">Get Back</a>
+        `
+        respuesta.forEach(user=>{
+            i++
+            userHTML+=`
+            <tr><td>${i}  </td> <td>  ${user.nombre}</td> <td>${user.correo}</td> <td>${user.producto}</td> </tr>
+            `
+        })
+
+        return res.send(`<table>
+            <tr>
+                <th>id </th>
+                <th>Nombre: </th>
+                <th>Correo</th>
+                <th>Producto </th>
+            </tr>
+            ${userHTML}
+            </table>`)
+    })
+})
+
+app.post('/upOrder',(req,res)=>{
+    let nameUser=req.body.nombreCliente;
+    let nomPedido=req.body.editProduct
+
+
+    con.query('UPDATE pedido SET producto=("'+nomPedido+'") WHERE nombre=("'+nameUser+'")',(err,respuesta,field)=>{
+        if(err) return console.log('ERROR:',err)
+
+        return res.send(`
+        <a href="pagOrder.html">Inicio</a>
+        <h1>Pedido de ${nameUser} cambiado a: ${nomPedido}</h1>
+        `)
+    })
+})
+
+
+app.listen(5000,()=>{
+    console.log("Servidor escuchando en el puerto 5000")
+})
